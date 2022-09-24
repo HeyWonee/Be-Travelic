1cacf0c1fb4a26d7e85c9e9fe272bdfe9c3ef58b
SooYoung <dearsyjang@naver.com>
2022-09-25 00:49:50 +0900

    [style] 댓글 모달창 배치 수정

--- Front/be-travelic/src/components/SNS/FeedPlace.tsx
+++ Front/be-travelic/src/components/SNS/FeedPlace.tsx
@@ -17,7 +17,7 @@ const FeedPlace = () => {
 
           <input
             type="text"
-            id="simple-search"
+            id="PlaceSearch"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="여행지 검색"
             required
